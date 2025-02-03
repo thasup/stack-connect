71a085df6b96b1c42b96643a140c0b5c803e5631
thasup <thanachonfirst@hotmail.com>
2025-02-03 11:39:17 +0700

    task: Update meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,64 @@
-import Image from "next/image";
+import { Container, Stack, Typography } from "@mui/material";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Stack
+      direction="column"
+      component="main"
+      sx={[
+        {
+          justifyContent: "center",
+          height: "calc((1 - var(--template-frame-height, 0)) * 100%)",
+          marginTop: "max(40px - var(--template-frame-height, 0px), 0px)",
+          minHeight: "100%"
+        }
+      ]}
+    >
+      <Stack
+        direction={{ xs: "column-reverse", md: "row" }}
+        sx={{
+          justifyContent: "center",
+          gap: { xs: 6, sm: 12 },
+          p: 2,
+        }}
+      >
+        <Stack
+          direction={{ xs: "column-reverse", md: "row" }}
+          sx={{
+            justifyContent: "flex-start",
+            gap: { xs: 6, sm: 12 },
+            p: { xs: 2, sm: 4 },
+            width: "100%"
+          }}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <Stack direction="column" gap={2}>
+            <Typography variant="h4">Select a game to play 🚀</Typography>
+            <Stack direction="row" spacing={1} flexWrap="wrap">
+              <Link href="/feelinks">
+                <Container
+                  maxWidth="lg"
+                  sx={{
+                    mt: 4,
+                    p: 4,
+                    border: "1px solid white",
+                    borderRadius: "8px",
+                    transition: "all 0.3s ease-in-out",
+                    "&:hover": {
+                      boxShadow: "0 0 20px 0 rgba(0, 191, 255, 0.8)",
+                      transform: "scale(1.05)"
+                    }
+                  }}
+                >
+                  <Stack direction="column" gap={2}>
+                    <Typography variant="h6">💖 Feelinks</Typography>
+                  </Stack>
+                </Container>
+              </Link>
+            </Stack>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Stack>
   );
 }
