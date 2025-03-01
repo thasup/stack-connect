8adc8ec15b4e172ed1e94dcef711e73c84ca7c2d
thasup <thanachonfirst@hotmail.com>
2025-03-02 02:27:38 +0700

    chore: Add svg icons

--- src/components/AppAppBar.tsx
+++ src/components/AppAppBar.tsx
@@ -1,20 +1,20 @@
-'use client';
+"use client";
 
-import * as React from 'react';
-import { alpha } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import Container from '@mui/material/Container';
-import Divider from '@mui/material/Divider';
-import MenuItem from '@mui/material/MenuItem';
-import Drawer from '@mui/material/Drawer';
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
-import ColorModeIconDropdown from '@/components/shared-theme/ColorModeIconDropdown';
-import Sitemark from '@/components/SitemarkIcon';
+import * as React from "react";
+import { alpha } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import Container from "@mui/material/Container";
+import Divider from "@mui/material/Divider";
+import MenuItem from "@mui/material/MenuItem";
+import Drawer from "@mui/material/Drawer";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import ColorModeIconDropdown from "@/components/shared-theme/ColorModeIconDropdown";
+import Sitemark from "@/components/SitemarkIcon";
 
 export default function AppAppBar() {
   const [open, setOpen] = React.useState(false);
@@ -29,9 +29,9 @@ export default function AppAppBar() {
       enableColorOnDark
       sx={{
         boxShadow: 0,
-        bgcolor: 'transparent',
-        backgroundImage: 'none',
-        mt: 'calc(var(--template-frame-height, 0px) + 28px)',
+        bgcolor: "transparent",
+        backgroundImage: "none",
+        mt: "calc(var(--template-frame-height, 0px) + 28px)"
       }}
     >
       <Container maxWidth="lg">
@@ -39,47 +39,38 @@ export default function AppAppBar() {
           variant="dense"
           disableGutters
           sx={(theme) => ({
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
             flexShrink: 0,
             borderRadius: `calc(${theme.shape.borderRadius}px + 8px)`,
-            backdropFilter: 'blur(24px)',
-            border: '1px solid',
+            backdropFilter: "blur(24px)",
+            border: "1px solid",
             borderColor: theme.palette.divider,
             backgroundColor: alpha(theme.palette.background.default, 0.4),
             boxShadow: theme.shadows[1],
-            padding: '8px 12px',
+            padding: "8px 12px"
           })}
         >
-          <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'center', px: 0 }}>
-            <Sitemark />
-            <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 1, display: "flex", alignItems: "center", px: 0 }}>
+            <Sitemark hasTitle />
+            <Box sx={{ display: { xs: "none", md: "flex" } }}>
               <Button variant="text" color="info" size="small">
-                Features
-              </Button>
-              <Button variant="text" color="info" size="small">
-                Testimonials
+                Games
               </Button>
               <Button variant="text" color="info" size="small">
-                Highlights
+                Resources
               </Button>
               <Button variant="text" color="info" size="small">
-                Pricing
-              </Button>
-              <Button variant="text" color="info" size="small" sx={{ minWidth: 0 }}>
-                FAQ
-              </Button>
-              <Button variant="text" color="info" size="small" sx={{ minWidth: 0 }}>
-                Blog
+                Contact
               </Button>
             </Box>
           </Box>
           <Box
             sx={{
-              display: { xs: 'none', md: 'flex' },
+              display: { xs: "none", md: "flex" },
               gap: 1,
-              alignItems: 'center',
+              alignItems: "center"
             }}
           >
             <Button color="primary" variant="text" size="small">
@@ -90,7 +81,7 @@ export default function AppAppBar() {
             </Button>
             <ColorModeIconDropdown />
           </Box>
-          <Box sx={{ display: { xs: 'flex', md: 'none' }, gap: 1 }}>
+          <Box sx={{ display: { xs: "flex", md: "none" }, gap: 1 }}>
             <ColorModeIconDropdown size="medium" />
             <IconButton aria-label="Menu button" onClick={toggleDrawer(true)}>
               <MenuIcon />
@@ -101,15 +92,15 @@ export default function AppAppBar() {
               onClose={toggleDrawer(false)}
               PaperProps={{
                 sx: {
-                  top: 'var(--template-frame-height, 0px)',
-                },
+                  top: "var(--template-frame-height, 0px)"
+                }
               }}
             >
-              <Box sx={{ p: 2, backgroundColor: 'background.default' }}>
+              <Box sx={{ p: 2, backgroundColor: "background.default" }}>
                 <Box
                   sx={{
-                    display: 'flex',
-                    justifyContent: 'flex-end',
+                    display: "flex",
+                    justifyContent: "flex-end"
                   }}
                 >
                   <IconButton onClick={toggleDrawer(false)}>
